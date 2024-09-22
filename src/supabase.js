89d2dbfb7bea1f33a75c68514badf80364f8b0fb
Pkankane18 <gautam1564p1@gmail.com>
2024-09-22 13:33:26 +0530

    added env

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = "https://jyssumxxzimhjkteedhc.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp5c3N1bXh4emltaGprdGVlZGhjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjY4NTA0MjMsImV4cCI6MjA0MjQyNjQyM30.2gOxLiCDTtgU8UJPjZTaJFj9hL_HrojJCCiV3xRkeTw";
+// const supabaseKey =
+//   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp5c3N1bXh4emltaGprdGVlZGhjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjY4NTA0MjMsImV4cCI6MjA0MjQyNjQyM30.2gOxLiCDTtgU8UJPjZTaJFj9hL_HrojJCCiV3xRkeTw";
+
+const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
